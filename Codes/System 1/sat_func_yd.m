80038c7a9f6fef10b3e6c78c8f28ee23e1be7fa6
Babak Esmaeili <160655225+Babak-Esmaeili@users.noreply.github.com>
2025-06-09 15:54:24 -0400

    Delete Codes directory

--- Codes/System 1/sat_func_yd.m
+++
@@ -1,5 +0,0 @@
-function yd_sat = sat_func_yd(yd,yd_l,yd_u)
-
-    yd_sat = (yd_l).*(yd<yd_l) + (yd).*(yd>=yd_l & yd<=yd_u) + (yd_u).*(yd>yd_u);
-
-end
--- Codes/System 1/sig_func.m
+++
@@ -1,6 +0,0 @@
-function out = sig_func(signal,power)
-
-%     out = (abs(signal).^power).*sign(signal);
-    out = (abs(signal).^power).*tanh(signal/0.01);
-
-end
